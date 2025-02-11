a901b2ecb1d154d80b23546f127063d62a435dd4
icaro armando <icaroarmando21@gmail.com>
2025-02-11 10:43:44 -0300

    refactor: improve dynamic scheduling loading

--- src/repositories/repository.appointment.js
+++ src/repositories/repository.appointment.js
@@ -13,9 +13,29 @@ join doctors d on (d.id_doctor = a.id_doctor)
 join users u on (u.id_user = a.id_user)
 join doctors_services ds on (ds.id_doctor = a.id_doctor and 
                         ds.id_service = a.id_service)
-where a.id_user = ?
-order by a.booking_date, a.booking_hour `;
+where a.id_appointment > 0 `;
 
+    if (id_user) {
+        filtro.push(id_user);
+        sql = sql + "and a.id_user = ? "
+    }
+
+    if (dt_start) {
+        filtro.push(dt_start);
+        sql = sql + "and a.booking_date >= ? "
+    }
+
+    if (dt_end) {
+        filtro.push(dt_end);
+        sql = sql + "and a.booking_date <= ? "
+    }
+
+    if (id_doctor) {
+        filtro.push(id_doctor);
+        sql = sql + "and a.id_doctor = ? "
+    }
+
+    sql = sql + "order by a.booking_date, a.booking_hour";
 
     const appointments = await query(sql, filtro);
 
@@ -35,13 +55,14 @@ async function Inserir(id_user,
     return appointment[0];
 }
 
-
 async function Excluir(id_user, id_appointment) {
 
-    let sql = `delete from appointments where id_appointment = ? and id_user = ? `;
+    let sql = `delete from appointments 
+    where id_appointment=? and id_user=?`;
 
     await query(sql, [id_appointment, id_user]);
 
     return { id_appointment };
 }
-export default { Listar, Inserir, Excluir };
+
+export default { Listar, Inserir, Excluir }
